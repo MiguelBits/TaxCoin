1dfe6fb660b8bd4f99e0effbe6d3e0bf7941f9eb
MiguelBits <mcv.cooper@gmail.com>
2023-08-30 10:16:22 +0100

    mocks

---
+++ test/MockupERC20.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.18;
+
+import "forge-std/Test.sol";
+
+import {ERC20} from "../src/utils/LPDiv.sol";
+
+contract MockupERC20 is ERC20 {
+    constructor(string memory _name) ERC20(_name, _name) {}
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) public {
+        _burn(from, amount);
+    }
+
+    function deposit() public payable {
+        _mint(msg.sender, msg.value);
+    }
+
+    function withdraw(uint256 amount) public {
+        _burn(msg.sender, amount);
+        payable(msg.sender).transfer(amount);
+    }
+}
