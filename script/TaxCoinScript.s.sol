2893c25d6026fab773dd426591fc525129ebfc5e
MiguelBits <mcv.cooper@gmail.com>
2023-08-30 12:40:36 +0100

    script

--- script/TaxCoinScript.s.sol
+++ script/TaxCoinScript.s.sol
@@ -11,12 +11,7 @@ contract TaxCoinScript is Script {
 
 
         // deploy TaxCoin
-        taxCoin = new TaxCoin(
-            0xdC887AE5c6052baDC5D17a4eB2350b309cb2025f, //dev wallet
-            0x9dC6821AE74FaAE71Dfd1016f14eAdcA7823Faf4, //token paired
-            0xa062aE8A9c5e11aaA026fc2670B0D65cCc8B2858, //router
-            0x5CFafe2E420544f2557aaF2C104778dFa6c4EF0D  //pool address
-        );
+        taxCoin = new TaxCoin("TaxCoiner");
         
         vm.stopBroadcast();
 
