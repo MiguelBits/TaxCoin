cdaa9fb07163ed6a4bee61357d8d04f9c12ca5a8
MiguelBits <mcv.cooper@gmail.com>
2023-08-30 14:01:01 +0100

    deploy

--- script/TaxCoinScript.s.sol
+++ script/TaxCoinScript.s.sol
@@ -5,6 +5,7 @@ import "forge-std/Script.sol";
 import {TaxCoin} from "../src/TaxCoin.sol";
 import {Coin} from "../src/Coin.sol";
 
+//forge script TaxCoinScript --broadcast --rpc-url $RPC --private-key $PRIVATE_KEY
 contract TaxCoinScript is Script {
     function run() public returns (Coin coin, TaxCoin taxCoin) {
         vm.startBroadcast();
@@ -13,7 +14,7 @@ contract TaxCoinScript is Script {
         coin = new Coin();
 
         // deploy TaxCoin
-        
+
         // taxCoin = new TaxCoin(
         //     0xdC887AE5c6052baDC5D17a4eB2350b309cb2025f, //dev wallet
         //     address(coin), //token in
